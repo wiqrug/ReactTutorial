92a3d24a8fb8f87e858bfbc4f68ed2b86237449d
wiqrug <papavasiliou.alexios@gmail.com>
2023-11-03 17:31:40 +0200

    following instructions and understanding the concepts

--- src/App.js
+++ src/App.js
@@ -27,19 +27,19 @@ export default function Board() {
     <>
     
       <div className="board-row">
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[0]}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
       </div>
       <div className="board-row">
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}></Square>
       </div>
       <div className="board-row">
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
       </div>
     </>
 
