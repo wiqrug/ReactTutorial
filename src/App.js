6cc300e1ca238881b7ad46f109db2dcd3c3ecb67
wiqrug <papavasiliou.alexios@gmail.com>
2023-11-03 17:29:21 +0200

    following steps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
+import {useState} from 'react';
+
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("x");
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
 export default function Board() {
+
+  //useState() call declares a squares state variable  that's initally set to the array that i created
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
+    
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square ></Square>
+        <Square ></Square>
+        <Square ></Square>
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square ></Square>
+        <Square ></Square>
+        <Square ></Square>
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square ></Square>
+        <Square ></Square>
+        <Square ></Square>
       </div>
     </>
 
